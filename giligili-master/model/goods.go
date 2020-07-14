47bd3a481cc9bc99d78e001509a82f196d008c8c
Akey <536481915@qq.com>
2020-07-14 21:08:36 +0800

    提交 model

---
+++ giligili-master/model/goods.go
@@ -0,0 +1,42 @@
+package model
+
+import "github.com/jinzhu/gorm"
+
+// GoodsSKU 商品sku
+type GoodsSKU struct {
+	gorm.Model
+	GoodsId    uint16
+	CreateTime string
+	UpdateTime string
+	Name       string
+	Content    string
+	Price      uint64
+	Unite      string
+	Image      string
+	Stock      uint64
+	Sales      string
+	Status     uint8
+	Goods      Goods
+	GoodsType  GoodsType
+}
+
+// Goods 商品spu
+type Goods struct {
+	gorm.Model
+	GoodsId    uint16
+	CreateTime string
+	UpdateTime string
+	Name       string
+	Content    string
+}
+
+// GoodsType 商品类型
+type GoodsType struct {
+	gorm.Model
+	GoodsId    uint16
+	CreateTime string
+	UpdateTime string
+	Name       string
+	Logo       string
+	Image      string
+}
